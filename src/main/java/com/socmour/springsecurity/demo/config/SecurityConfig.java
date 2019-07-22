200e3ebfd32714894e998e403814750d2a2f9b9f
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-22 15:54:53 +0300

    added security

---
+++ src/main/java/com/socmour/springsecurity/demo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.socmour.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // in memory authentication
+
+        UserBuilder userBinder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBinder.username("John").password("test123").roles("EMPLOYEE"))
+                .withUser(userBinder.username("Mary").password("test123").roles("MANAGER"))
+                .withUser(userBinder.username("Mark").password("test123").roles("ADMIN"));
+
+    }
+}
