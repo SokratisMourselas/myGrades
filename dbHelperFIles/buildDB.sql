ddd0dbb7715b2ceea566b1f990217af0f3a35829
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-24 12:41:08 +0300

    db update

--- dbHelperFIles/buildDB.sql
+++ dbHelperFIles/buildDB.sql
@@ -1,57 +1,59 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.3.10-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
+DROP DATABASE  IF EXISTS `spring_security_demo_bcrypt`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+CREATE DATABASE  IF NOT EXISTS `spring_security_demo_bcrypt`;
+USE `spring_security_demo_bcrypt`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users`
+VALUES
+('john','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1),
+('mary','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1),
+('susan','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1);
 
 
--- Dumping database structure for mygradesdb
-CREATE DATABASE IF NOT EXISTS `mygradesdb` /*!40100 DEFAULT CHARACTER SET ascii */;
-USE `mygradesdb`;
+--
+-- Table structure for table `authorities`
+--
 
--- Dumping structure for table mygradesdb.authorities
-CREATE TABLE IF NOT EXISTS `authorities` (
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL,
   UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
   CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table mygradesdb.authorities: ~5 rows (approximately)
-DELETE FROM `authorities`;
-/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
-INSERT INTO `authorities` (`username`, `authority`) VALUES
-	('john', 'ROLE_EMPLOYEE'),
-	('mark', 'ROLE_ADMIN'),
-	('mark', 'ROLE_EMPLOYEE'),
-	('mary', 'ROLE_EMPLOYEE'),
-	('mary', 'ROLE_MANAGER');
-/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
+--
+-- Dumping data for table `authorities`
+--
 
--- Dumping structure for table mygradesdb.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `enabled` int(1) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `authorities`
+VALUES
+('john','ROLE_EMPLOYEE'),
+('mary','ROLE_EMPLOYEE'),
+('mary','ROLE_MANAGER'),
+('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_ADMIN');
 
--- Dumping data for table mygradesdb.users: ~3 rows (approximately)
-DELETE FROM `users`;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
-	('john', '{noop}test123', 1),
-	('mark', '{noop}test123', 1),
-	('mary', '{noop}test123', 1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
