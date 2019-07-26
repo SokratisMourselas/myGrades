e39bc38fd197805b686e2d71b9abe67bd4ff079f
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-26 09:31:36 +0300

    fixed datasource properties

--- src/main/java/com/socmour/springsecurity/demo/config/Config/AppConfig.java
+++ src/main/java/com/socmour/springsecurity/demo/config/Config/AppConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -61,10 +62,10 @@ public class AppConfig implements WebMvcConfigurer {
             myDataSource.setUser(env.getProperty("jdbc.user"));
             myDataSource.setPassword(env.getProperty("jdbc.password"));
 
-            myDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
-            myDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
-            myDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
-            myDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+            myDataSource.setInitialPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.initialPoolSize"))));
+            myDataSource.setMinPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.minPoolSize"))));
+            myDataSource.setMaxPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.maxPoolSize"))));
+            myDataSource.setMaxIdleTime(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.maxIdleTime"))));
 
 
         } catch (PropertyVetoException e) {
