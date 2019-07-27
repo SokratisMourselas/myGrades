bfff0a6fc538b236415305ebfb0564dfb3ed5d2f
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-27 19:40:49 +0300

    updated db

--- dbHelperFIles/buildDB.sql
+++ dbHelperFIles/buildDB.sql
@@ -1,59 +1,78 @@
-DROP DATABASE  IF EXISTS `spring_security_demo_bcrypt`;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.3.10-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
 
-CREATE DATABASE  IF NOT EXISTS `spring_security_demo_bcrypt`;
-USE `spring_security_demo_bcrypt`;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` char(68) NOT NULL,
-  `enabled` tinyint(1) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
--- NOTE: The passwords are encrypted using BCrypt
---
--- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
---
--- Default passwords here are: fun123
---
-
-INSERT INTO `users`
-VALUES
-('john','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1),
-('mary','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1),
-('susan','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
---
--- Table structure for table `authorities`
---
+-- Dumping database structure for mygradesdb
+CREATE DATABASE IF NOT EXISTS `mygradesdb` /*!40100 DEFAULT CHARACTER SET ascii */;
+USE `mygradesdb`;
 
-DROP TABLE IF EXISTS `authorities`;
-CREATE TABLE `authorities` (
+-- Dumping structure for table mygradesdb.authorities
+CREATE TABLE IF NOT EXISTS `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) DEFAULT NULL,
+  UNIQUE KEY `username_authority` (`username`,`authority`),
+  KEY `FK_authorities_roles` (`authority`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
---
--- Dumping data for table `authorities`
---
+-- Dumping data for table mygradesdb.authorities: ~5 rows (approximately)
+DELETE FROM `authorities`;
+/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
+INSERT INTO `authorities` (`username`, `authority`, `id`) VALUES
+	('john', 'ROLE_EMPLOYEE', NULL),
+	('mark', 'ROLE_EMPLOYEE', NULL),
+	('mark', 'ROLE_MANAGER', NULL),
+	('mary', 'ROLE_ADMIN', NULL),
+	('mary', 'ROLE_EMPLOYEE', NULL);
+/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
 
-INSERT INTO `authorities`
-VALUES
-('john','ROLE_EMPLOYEE'),
-('mary','ROLE_EMPLOYEE'),
-('mary','ROLE_MANAGER'),
-('susan','ROLE_EMPLOYEE'),
-('susan','ROLE_ADMIN');
+-- Dumping structure for table mygradesdb.students
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(60) NOT NULL,
+  `lastName` varchar(60) NOT NULL,
+  `dateOfBirth` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
+-- Dumping data for table mygradesdb.students: ~0 rows (approximately)
+DELETE FROM `students`;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
 
+-- Dumping structure for table mygradesdb.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` char(80) NOT NULL,
+  `enabled` int(1) DEFAULT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table mygradesdb.users: ~4 rows (approximately)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `password`, `enabled`, `firstName`, `lastName`, `email`) VALUES
+	(1, 'john', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 1, '', '', ''),
+	(2, 'mark', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 1, '', '', ''),
+	(3, 'mary', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 1, '', '', ''),
+	(4, 'sokratis', '$2a$10$rvmhPXkK5F9ZqGgJczu/8uP0T99BYh8kZud6Tmq.ImLP/3/srWW/K', NULL, 'test', NULL, NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
